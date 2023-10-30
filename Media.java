b0f5543409c8172f3b76a96c90efa0bcfe8dfb2e
unknown <Felipe da Silva>
2023-10-29 22:36:31 -0300

    Adicionando verificações e corrigindo as saídas de dados

--- Media.java
+++ Media.java
@@ -9,7 +9,12 @@ public class Media {
 
     public void adicionaNotas() {
         for (int i = 0; i < quantAvaliacoes; i++) {
-            System.out.println("Digite a nota da avaliação" + (i + 1) + ":");// i + 1 pq se fosse só i seria 0
+            System.out.println("Digite a nota da avaliação " + (i + 1) + ":");// i + 1 pq se fosse só i seria 0
+            if(!scanner.hasNextFloat() || scanner.nextFloat() < 0){
+                System.out.println("Por favor, digite um número racional positivo e válido (lembre-se de usar vírgula ',' em vez do ponto '.')");
+                System.out.println("Digite a nota da avaliação " + (i + 1) + ":");
+                scanner.next();
+            }
             notas[i] = scanner.nextFloat();// Como vai receber um dado flututante , usamos nextFloat
             somaNotas += notas[i];
         }
@@ -21,10 +26,12 @@ public class Media {
     }
 
     public void mostraNotas() {
+        System.out.println("");
+        System.out.println("");
         for (int i = 0; i < quantAvaliacoes; i++) {
             System.out.println("Nota da " + (i + 1) + "° avaliação: " + notas[i]);
         }
-        System.out.println("Esta foi a sua média das avaliações nesta unidade" + somaNotas / quantAvaliacoes);
+        System.out.printf("Esta foi a sua média nesta unidade  %.2f\n", (somaNotas / quantAvaliacoes));
     }
 
     public static void main(String[] args) {

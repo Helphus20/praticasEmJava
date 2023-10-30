adc5bdd93cdc2024b379331f2316c8c5afe1b73d
unknown <Felipe da Silva>
2023-10-29 21:44:58 -0300

    Simplificando e ordenando corretamente o código

--- Media.java
+++ Media.java
@@ -5,36 +5,34 @@ public class Media {
     float[] notas;
     float somaNotas = 0;
     String nome;
+    Scanner scanner = new Scanner(System.in);
 
     public void adicionaNotas() {
-        try (Scanner scanner = new Scanner(System.in)) {
-            for (int i = 0; i < quantAvaliacoes; i++) {
-                System.out.println("Digite a nota " + (i + 1) + ":");// i + 1 pq se fosse só i seria 0
-                notas[i] = scanner.nextFloat();//Como vai receber um dado flututante , usamos nextInt
-                somaNotas += notas[i];
-            }
-        }
-        System.out.println("Esta foi a sua média das avaliações nesta unidade" + somaNotas/quantAvaliacoes);
-    }
-    public void quantAvaliacoes(){
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.println("Quantas avaliações você fez nessa unidade?");
-            quantAvaliacoes = scanner.nextInt();//Como vai receber um dado inteiro, usamos nextInt
+        for (int i = 0; i < quantAvaliacoes; i++) {
+            System.out.println("Digite a nota da avaliação" + (i + 1) + ":");// i + 1 pq se fosse só i seria 0
+            notas[i] = scanner.nextFloat();// Como vai receber um dado flututante , usamos nextFloat
+            somaNotas += notas[i];
         }
     }
-    public void mostraNotas(){
-        for(int i = 0; i < quantAvaliacoes; i++){
+
+    public void quantAvaliacoes() {
+        System.out.println("Quantas avaliações você fez nessa unidade?");
+        quantAvaliacoes = scanner.nextInt();// Como vai receber um dado inteiro, usamos nextInt
+    }
+
+    public void mostraNotas() {
+        for (int i = 0; i < quantAvaliacoes; i++) {
             System.out.println("Nota da " + (i + 1) + "° avaliação: " + notas[i]);
         }
-        System.out.println("Média na unidade: " + somaNotas/quantAvaliacoes);
+        System.out.println("Esta foi a sua média das avaliações nesta unidade" + somaNotas / quantAvaliacoes);
     }
+
     public static void main(String[] args) {
         Media boletim = new Media();
-        boletim.quantAvaliacoes = 3; // quantidade de unidades
+        boletim.quantAvaliacoes();
         boletim.notas = new float[boletim.quantAvaliacoes];
-        boletim.quantAvaliacoes();
         boletim.adicionaNotas();
-        boletim.quantAvaliacoes();
+        boletim.mostraNotas();
     }
 }
 
